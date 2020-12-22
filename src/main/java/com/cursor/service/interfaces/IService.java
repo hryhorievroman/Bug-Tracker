6d50f10bb4735a3e4f1dc3bfa6663c5688888a2b
Juliya <ulcakopitko@gmail.com>
2020-12-22 23:19:29 +0200

    Add interface IService

---
+++ src/main/java/com/cursor/service/interfaces/IService.java
@@ -0,0 +1,17 @@
+package com.cursor.service.interfaces;
+
+import com.cursor.service.exceptions.*;
+
+import java.util.List;
+
+public interface IService<T> {
+    void create(T entity) throws BadRequestException;
+
+    List<T> getAll();
+
+    T findById() throws NotFoundException;
+
+    void edit(int id, T entity) throws BadRequestException, NotFoundException;
+
+    void delete(int id) throws NotFoundException;
+}
