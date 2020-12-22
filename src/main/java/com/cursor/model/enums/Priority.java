55a1697e583fff61a60ba79319a7cc68e18018e9
Roman Bielyi <romanbielyi.amor@gmail.com>
2020-12-22 16:14:17 +0200

    Added Status and Priority enums

---
+++ src/main/java/com/cursor/model/enums/Priority.java
@@ -0,0 +1,9 @@
+package com.cursor.model.enums;
+
+public enum Priority {
+    TRIVIAL,
+    MINOR,
+    MAJOR,
+    CRITICAL,
+    BLOCKER,
+}
---
+++ src/main/java/com/cursor/model/enums/Status.java
@@ -0,0 +1,9 @@
+package com.cursor.model.enums;
+
+public enum Status {
+    TODO,
+    IN_PROGRESS,
+    IN_REVIEW,
+    APPROVED,
+    DONE
+}
