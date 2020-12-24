2ed704380b6ff112fd3566279f1437b532a5a264
Barburon <lamis.htc@gmail.com>
2020-12-24 22:12:36 +0200

    Fixed empty's lines

---
+++ src/main/java/com/cursor/view/Actions.java
@@ -0,0 +1,25 @@
+package com.cursor.view;
+
+import com.cursor.view.interfaces.Menu;
+
+import java.util.Scanner;
+
+public class Actions implements Menu {
+    Scanner scanner = new Scanner(System.in);
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void logOut() {
+
+    }
+
+    @Override
+    public void back() {
+
+    }
+}
--- src/main/java/com/cursor/view/interfaces/Menu.java
+++ src/main/java/com/cursor/view/interfaces/Menu.java
@@ -3,7 +3,6 @@ package com.cursor.view.interfaces;
 public interface Menu {
     void show();
     void back();
-
     default void logOut() {
         System.out.println("You are logged out of the Bug Tracker");
         System.exit(0);
