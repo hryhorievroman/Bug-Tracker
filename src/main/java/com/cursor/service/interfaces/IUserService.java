b50ecbcb6f5dff791a689ecf897bf0e85748cf79
Juliya <ulcakopitko@gmail.com>
2020-12-25 16:25:42 +0200

    Add interface IUserService

---
+++ src/main/java/com/cursor/service/interfaces/IUserService.java
@@ -0,0 +1,12 @@
+package com.cursor.service.interfaces;
+
+import com.cursor.model.User;
+import com.cursor.service.exceptions.BadRequestException;
+
+public interface IUserService extends IService<User> {
+
+    void registerUser(User user) throws BadRequestException;
+
+    void loginUser(String username, String password) throws BadRequestException;
+
+}
